04433848d7fcaf0a7a04a3ab80a622fd84da1b6e
TheLucyClub Github Team <thelucyclub@gmail.com>
2018-03-24 15:04:50 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -577,7 +577,7 @@ rule34 `+"`"+"ONLINE"+"`"+`
     };
 	if (message.content == "Seb, dblinfo"){
 		cmd = true;
-		/*message.channel.startTyping();
+		message.channel.startTyping();
         //request({url: 'https://discordapp.com/api/users/@me/guilds',headers: {'Authorization': 'Bot NDA4NzE4Mjk3NDAwNDc1NjY4.DVUleg.VJV1fHSXPvXV_TX3CtJor-oAX8I'}};, function (error, response, body){
             message.reply({embed:{
                 color: 3394815,
@@ -591,10 +591,7 @@ rule34 `+"`"+"ONLINE"+"`"+`
                     icon_url: `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png`
                 }
             }});
-        //});*/
-		message.reply({attachment:{
-			url: "https://discordbots.org/api/widget/408718297400475668.svg"
-		}});
+        //});
 		message.channel.stopTyping();
     };
 	if (message.content.substr(0,9) == "Seb, gif "){
@@ -650,7 +647,7 @@ rule34 `+"`"+"ONLINE"+"`"+`
 			  //var count = Math.floor((Math.random() * reply.posts.post.length))
 			  //var file = `reply.posts.post[count].$.file_url`
 			  console.log(file)
-			  /*message.reply({embed:{
+			  message.reply({embed:{
 				color: 3394815,
 				title: "rule34",
 				url: file,
@@ -663,12 +660,7 @@ rule34 `+"`"+"ONLINE"+"`"+`
 					text: `Requested by ${message.author.username}`,
 					icon_url: `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png`
 				}
-			  }});*/
-			  message.reply({
-				  url: file,
-				  height: dimensionY,
-				  width: dimensionX
-			  });
+			  }});
 			};
 		});
 		/*
