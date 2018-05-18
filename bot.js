aed048db19017d5596f5e029c5c76dd50895b06b
TheLucyClub Github Team <thelucyclub@gmail.com>
2018-05-17 21:09:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1121,7 +1121,7 @@ rule34 `+"`"+"ONLINE"+"`"+`
 	if (message.content.substr(0,4) == "SEB,"){
 		message.reply("Whoops! The prefix is `Seb,` (case sensitive)");
 	};
-	console.log("[" + message.member.guild.name + "]: {Author " + message.author.username + ", Message '" + message.content + "', Command " + cmd + "}");
+	console.log("[" + message.member.guild.name + "]: {Author " + message.author.username + ", Message '" + message.content + "', Embed " + message.embed + ", Command " + cmd + "}");
 ///////////////////////////////////////////////////////////////
 } catch(err) {
 	console.log(`=== [ Error Encountered ] ===\n\n<${err.line}>: ${err.message}\n\n=================`);
