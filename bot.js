463ded161a651639a7275a90d2b3bd6192657c05
TheLucyClub Github Team <thelucyclub@gmail.com>
2018-05-20 10:52:54 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1235,9 +1235,10 @@ rule34 `+"`"+"ONLINE"+"`"+`
 			.toBuffer('PNG',function (err, buffer) {
 			  if (err) console.log("!!!!!!!!!!!!!!!  " + err); return;
 			  process.env['output-' + message.author.id + '.buf'] = buffer.toString('utf-8');
-			  fs.writeFileSync('/tmp/' + name, buffer, function(err) {
+			  fs.writeFileSync('/tmpimg.png', function(err) {
 			      if(err) {
-			  	return console.log(err);
+			  	console.log(err);
+				return;
 			      }
 			      message.reply("Success, reading file contents...");
 			  }); 
@@ -1246,7 +1247,7 @@ rule34 `+"`"+"ONLINE"+"`"+`
 			//	if (err) console.log("!!!!!!!!!!!!!!  " + err); message.reply("crazyimg failed"); return;
 			//});
 		//fs.createReadStream('output-' + message.author.id + '.jpg');
-		message.reply({files:['/tmp/' + name]}).catch(console.error);
+		message.reply({files:['/tmpimg.png']}).catch(console.error);
 		//message.reply(process.env['output-' + message.author.id + '.buf']);
 		message.channel.send("crazyimg complete!");
 		message.channel.stopTyping(true);
