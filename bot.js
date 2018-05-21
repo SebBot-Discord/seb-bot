1349c9ea9a5bf455a8cf94854b724375328ba54d
TheLucyClub Github Team <thelucyclub@gmail.com>
2018-05-21 14:06:47 -0400

    Hopefully no more crashing.

--- bot.js
+++ bot.js
@@ -1383,3 +1383,12 @@ console.log(`refreshed seb bot (${count})`);
 }catch(err){console.log(`=== [ Error Encountered ] ===\n\n${err.message}\n\n=================`);run();};};
 run();
 //setInterval(run,60000);
+process.on("uncaughtException", (err) => {
+  const errorMsg = err.stack.replace(new RegExp(`${__dirname}/`, "g"), "./");
+  client.logger.error(`Uncaught Exception: ${errorMsg}`);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", err => {
+  client.logger.error(`Unhandled rejection: ${err}`);
+});
