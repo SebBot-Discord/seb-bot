1b1f00be182858e2ac3cf9705cda0a2e0967025a
TheLucyClub Github Team <thelucyclub@gmail.com>
2018-05-22 16:43:34 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1462,7 +1462,7 @@ rule34 `+"`"+"ONLINE"+"`"+`
 			return;
 	}
 ///////////////////////////////////////////////////////////////
-	if (message.content.substr(0, 5) == "Seb, "){
+	if (message.content.startsWith("Seb,")){
 	  console.log("[" + message.member.guild.name + " @ " + message.channel.name + "]: {Author " + message.author.username + ", Bot? " + message.author.bot + ", Message '" + message + "', MessageLength " + message.content.length);
 	}
 ///////////////////////////////////////////////////////////////
